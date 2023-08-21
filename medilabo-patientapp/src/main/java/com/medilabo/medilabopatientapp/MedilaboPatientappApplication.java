7de869798e69cf6b94f56d08518522242f4cd7b4
Sophie LAURENT <sophielaurent92@gmail.com>
2023-08-21 14:16:38 +0200

    Initial commit

---
+++ medilabo-patientapp/src/main/java/com/medilabo/medilabopatientapp/MedilaboPatientappApplication.java
@@ -0,0 +1,13 @@
+package com.medilabo.medilabopatientapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedilaboPatientappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedilaboPatientappApplication.class, args);
+	}
+
+}
---
+++ medilabo-patientapp/src/test/java/com/medilabo/medilabopatientapp/MedilaboPatientappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medilabo.medilabopatientapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedilaboPatientappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
