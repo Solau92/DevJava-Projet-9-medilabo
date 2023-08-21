588c66d9431cfb826bdf9a0e36d70f2592f743f8
Sophie LAURENT <sophielaurent92@gmail.com>
2023-08-21 21:14:33 +0200

    Initial commit

---
+++ medilabo-frontapp/src/main/java/com/medilabo/medilabofrontapp/MedilaboFrontappApplication.java
@@ -0,0 +1,13 @@
+package com.medilabo.medilabofrontapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedilaboFrontappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedilaboFrontappApplication.class, args);
+	}
+
+}
---
+++ medilabo-frontapp/src/test/java/com/medilabo/medilabofrontapp/MedilaboFrontappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medilabo.medilabofrontapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedilaboFrontappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
