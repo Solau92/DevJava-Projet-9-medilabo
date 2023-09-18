5a2f4dca52fe6aca9b736ffec9a0479e830806b8
Sophie LAURENT <sophielaurent92@gmail.com>
2023-09-18 13:20:51 +0200

    Docker compose for patient app

---
+++ medilabo-patientapp/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO patient (firstName, lastName, dateOfBirth, gender, address, phoneNumber) VALUES 
+('Sophie', 'LAURENT', '1984-01-01', 'F', '92400 COURBEVOIE', 0123456789),
+('Julien', 'DUPONT', '1985-09-22', 'M', '75001 PARIS', 0123456789),
+('Marie', 'DURAND', '1932-11-30', 'F', '75017 PARIS', 0123456789),
+('Marc', 'MARTIN', '1979-02-25', 'M', '75011 PARIS', 0123456789),
+('Alain', 'THOMAS', '1981-06-10', 'Other', '75018 PARIS', 0123456789);
